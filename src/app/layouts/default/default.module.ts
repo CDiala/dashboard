e053a92103499138e472888c79ab225ad1b8dd91
Chibuzo Diala <dialachibuzo@yahoo.com>
2021-02-17 03:30:08 +0100

    Created shared components and default component, and linked them up. Routing was also set up.

---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,21 @@
+import { DashboardComponent } from './../../modules/dashboard/dashboard.component';
+import { DefaultComponent } from './default.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "src/app/shared/shared.module";
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule
+  ]
+})
+export class DefaultModule { }
